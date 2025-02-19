1f6f136b686e167bcd8c786c6454ba99315a24ff
Vinodhinichokkan <vinodhinichokkan99@gmail.com>
2025-02-19 14:47:20 +0530

    exe

---
+++ flattenarray.js
@@ -0,0 +1,19 @@
+const nestedArray = [1,[2,[3,4],5],6,[7,[8,9]]];
+
+
+function flattenArray(arr){
+    let flatArr = [];
+
+    arr.forEach(item => {
+        if (Array.isArray(item)){
+            flatArr = flatArr.concat(flattenArray(item))
+        }else{
+            flatArr.push(item);
+        }
+    });
+    return flatArr;
+}
+
+console.log(flattenArray(nestedArray))
+
+//[1,2,3,4,5,6,7,8,9]
--- missingnumber.js
+++ missingnumber.js
@@ -1,8 +1,28 @@
+ //----------- USING Sum formula method-------------
+ 
  function findMissingNumber(arr){
     const n = arr.length + 1;
-    const totalSum = (n*(n+1))/2;
+    //sum of numbers from 1 to n
+    const totalSum = (n*(n+1))/2; //15
     //sum of numbers in array
-    const arraySum = arr.reduce((acc,num)=>acc + num,0);
+    const arraySum = arr.reduce((acc,num)=>acc + num,0); //12
+
+    //15-12
     return totalSum - arraySum; //MissingNumber
  }
- console.log(findMissingNumber)
+ console.log(findMissingNumber[1,2,4,5])   //3
+
+ //--------------USING SET METHOD---------------------
+
+function findMissingNumber(arr){
+   const n = arr.length + 1; //5
+   const numberSet = new Set(arr);
+for (let i = 1; i<=n; i++){
+      if(!numberSet.has(i)){
+         return i;
+      }
+   }
+}
+
+console.log(findMissingNumber([1,3,4,5]))
+
