3f4a6b0fd144c2f11156fb3b29bc5acb4057d608
Vinodhinichokkan <vinodhinichokkan99@gmail.com>
2025-03-17 14:43:14 +0530

    exe

---
+++ math.js
@@ -0,0 +1,18 @@
+const num = [4, 9, 16, 25]
+const newArr = num.map(Math.sqrt);
+
+console.log(newArr);    //[ 2, 3, 4, 5 ]
+
+//----------------------
+
+const number = [1, 2, 3, 4, 5]
+const newArray = number.map(num => num * 2);
+
+console.log(newArray);     //[ 2, 4, 6, 8, 10 ]
+ 
+//---------------------------------
+
+const number1 = [25, 36, 49, 81]
+const newArr1 = number1.map(Math.sqrt);
+
+console.log(newArr1)  //[ 5, 6, 7, 9]
